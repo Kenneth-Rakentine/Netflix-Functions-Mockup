069e54aa35c837f57743ba63488c89ff3dfe4c11
Kenny Rakentine <specialistmorgenj@gmail.com>
2023-06-15 01:39:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+let changeBkg = ()=>{
+    let switched = document.querySelector('.hanna')
+    switched.setAttribute('src','https://i.gifer.com/S14x.gif')
+}
+
+// let infoSwap = ()=>{
+//     let swap = document.querySelector('.hannaTtitle')
+//     swap.innerHTML = "MANIFEST"
+// }
